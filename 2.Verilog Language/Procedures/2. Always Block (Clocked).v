58239347050e052f3f306694ea60677361836173
NoobLearnTV <p.nayak.911@gmail.com>
2024-07-27 22:22:02 +0530

    Update 2. Always Block (Clocked).v

--- 2.Verilog Language/Procedures/2. Always Block (Clocked).v
+++ 2.Verilog Language/Procedures/2. Always Block (Clocked).v
@@ -7,4 +7,12 @@ module top_module(
     output reg out_always_comb,
     output reg out_always_ff   );
 
+    assign out_assign = a ^ b;
+    
+    always @*
+        out_always_comb = a ^ b;
+    
+    always @ (posedge clk)
+        out_always_ff <= a ^ b;
+    
 endmodule
